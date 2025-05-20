f3b82e121075e5724cca0823b38696527b9e3e0b
NatanNet <natanlaptop8@gmail.com>
2025-05-20 16:47:12 +0700

    .env

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Route::middleware('api')->group(function () {
     Route::post('/pendaftar', [App\Http\Controllers\Api\PendaftarController::class, 'store']);
     Route::get('/events', [App\Http\Controllers\Api\EventController::class, 'index']);
 });
+
