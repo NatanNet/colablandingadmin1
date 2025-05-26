f6dfcd0dda510b256b7f17014e17d125b2bdc678
NatanNet <natanlaptop8@gmail.com>
2025-05-27 00:11:52 +0700

    penambahan kembali si login dan Delete di kegiatan

--- app/Filament/Resources/KegiatanResource.php
+++ app/Filament/Resources/KegiatanResource.php
@@ -97,6 +97,7 @@ class KegiatanResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
